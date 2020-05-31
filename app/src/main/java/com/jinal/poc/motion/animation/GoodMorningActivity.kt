2d9017b1c321067a99aba166b493869dc04e2925
jinal tandel <jinal.tandel@nl.abnamro.com>
2020-05-31 12:40:45 +0200

    Adding motion to layout

---
+++ app/src/main/java/com/jinal/poc/motion/animation/GoodMorningActivity.kt
@@ -0,0 +1,11 @@
+package com.jinal.poc.motion.animation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoodMorningActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_good_morning)
+    }
+}
